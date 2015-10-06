fb2076863d5373af0f1a4384dc216faf3d115ca8
Laurent Treguier <laurent.treguier@isen.fr>
2015-10-06 17:58:35 +0200

    Added views

---
+++ views/login.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <form method = "POST">
+            <input type = "text" placeholder = "identifier"/>
+            <input type = "password" placeholder = "password"/>
+            <input type = "submit" value = "login"/>
+        </form>
+    </body>
+</html>
