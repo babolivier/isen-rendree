61ab63017aa5038c48723714fb4a639d99272936
Brendan Abolivier <contact@brendanabolivier.com>
2015-12-03 18:06:15 +0100

    :-) :-) :-)

--- views/login.html.php
+++ views/login.html.php
@@ -1,21 +1,5 @@
-<?php
-
-if(isset($error))
-{
-    if($error)
-        echo "<h1>T'es mauvais Jack</h1>";
-    else
-    {
-?>
-
-<h1>T'es connecté gros</h1>
-
-<?php }} else { ?>
-
 <form method="POST">
     <input type="text" placeholder="identifier" name="login"/>
     <input type="password" placeholder="password" name="password"/>
     <input type="submit" value="login"/>
 </form>
-
-<?php } ?>
