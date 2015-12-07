354b0c4103cb612ed3bac1ab28c793d194c98a1c
LaurentTreguier <laurent.treguier@isen.fr>
2015-12-07 15:31:08 +0100

    Export login code to layout

--- views/layout.html.php
+++ views/layout.html.php
@@ -6,8 +6,26 @@
         <title>
             <?php echo $title; ?>
         </title>
+        
+        <link href="lib/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="lib/fonts/css/font-awesome.min.css" rel="stylesheet">
+        <link href="lib/css/animate.min.css" rel="stylesheet">
+
+        <link href="lib/css/custom.css" rel="stylesheet">
+        <link href="lib/css/icheck/flat/green.css" rel="stylesheet">
+
+
+        <script src="lib/js/jquery.min.js"></script>
+
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
-    <body>
-        <?php echo $content; ?>
+    <body style="background:#F7F7F7;">
+        <ul>
+            <?php echo $content; ?>
+        </ul>
     </body>
 </html>
--- views/login.html.php
+++ views/login.html.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-
-    <!-- Bootstrap core CSS -->
-
-    <link href="lib/css/bootstrap.min.css" rel="stylesheet">
-
-    <link href="lib/fonts/css/font-awesome.min.css" rel="stylesheet">
-    <link href="lib/css/animate.min.css" rel="stylesheet">
-
-    <!-- Custom styling plus plugins -->
-    <link href="lib/css/custom.css" rel="stylesheet">
-    <link href="lib/css/icheck/flat/green.css" rel="stylesheet">
-
-
-    <script src="lib/js/jquery.min.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body style="background:#F7F7F7;">
-
 <div class="">
     <div id="wrapper">
         <div id="login" class="animate form">
@@ -53,13 +23,7 @@
                         </div>
                     </div>
                 </form>
-                <!-- form -->
             </section>
-            <!-- content -->
         </div>
     </div>
 </div>
-
-</body>
-
-</html>
